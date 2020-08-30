f688c60e8427f91758ef344b14f7eddab42c6c20
siyoon <siyoon210@gmail.com>
2020-08-31 08:31:56 +0900

    feat: Implement JsoupOgMetaElementHtmlParser

--- src/main/java/org/ogparser4j/htmlparser/OgMetaElement.java
+++ src/main/java/org/ogparser4j/htmlparser/OgMetaElement.java
@@ -8,4 +8,30 @@ public class OgMetaElement {
         this.property = property;
         this.content = content;
     }
+
+    @Override
+    public String toString() {
+        return "OgMetaElement{" +
+                "property='" + property + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OgMetaElement)) return false;
+
+        OgMetaElement that = (OgMetaElement) o;
+
+        if (!property.equals(that.property)) return false;
+        return content.equals(that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = property.hashCode();
+        result = 31 * result + content.hashCode();
+        return result;
+    }
 }
---
+++ src/test/java/org/ogparser4j/htmlparser/JsoupOgMetaElementHtmlParserTest.java
@@ -0,0 +1,29 @@
+package org.ogparser4j.htmlparser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class JsoupOgMetaElementHtmlParserTest {
+    private static final String SAMPLE_URL = "https://ogp.me/";
+    private JsoupOgMetaElementHtmlParser jsoupOgMetaElementHtmlParser;
+
+    @BeforeEach
+    public void init() {
+        jsoupOgMetaElementHtmlParser = new JsoupOgMetaElementHtmlParser();
+    }
+
+    @Test
+    public void jsoupParserTest() {
+        final List<OgMetaElement> ogMetaElements = jsoupOgMetaElementHtmlParser.getOgMetaElements(SAMPLE_URL);
+        for (OgMetaElement ogMetaElement : ogMetaElements) {
+            System.out.println(ogMetaElement.toString());
+        }
+        assertThat(ogMetaElements.contains(new OgMetaElement("title", "Open Graph protocol"))).isTrue();
+        assertThat(ogMetaElements.contains(new OgMetaElement("type", "website"))).isTrue();
+        assertThat(ogMetaElements.contains(new OgMetaElement("url", "https://ogp.me/"))).isTrue();
+    }
+}
